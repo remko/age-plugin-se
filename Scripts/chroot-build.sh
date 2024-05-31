dac69e96a091236c7bd45ec184517f7a8fbc2994
Remko Tronçon <remko@el-tramo.be>
2024-05-31 18:29:24 +0200

    chroot-build: Support x86-64

--- Scripts/chroot-build.sh
+++ Scripts/chroot-build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -x 
 
+################################################################################
+# Builds age-plugin-se in a Debian chroot
+#
+# Has to be run as root
+################################################################################
+
 set -e
 
 NAME=age-plugin-se
@@ -9,16 +15,25 @@ if [ -z "$PREFIX"]; then
   PREFIX=/usr/local
 fi
 if [ -z "$DESTDIR"]; then
-  DESTDIR=build
+  DESTDIR=.build/chroot-build
 fi
 
+ARCH=$(uname -m)
 CHROOT_DIR=/mnt/$NAME-build-chroot
-SWIFT_PACKAGE_URL=https://download.swift.org/swift-5.10-release/ubuntu2204-aarch64/swift-5.10-RELEASE/swift-5.10-RELEASE-ubuntu22.04-aarch64.tar.gz
-SWIFT_DIR=/usr/local/swift-5.10-RELEASE-ubuntu22.04-aarch64
-SWIFT=$SWIFT_DIR/usr/bin/swift
 CHROOT_BUILD_DIR=/opt/build
-LINKER=ld-linux-aarch64.so.1
 SOURCES="Sources Tests Package.swift"
+
+if [ "$ARCH" == "aarch64" ]; then
+  SWIFT_PACKAGE_SUFFIX=-aarch64
+  LINKER=ld-linux-aarch64.so.1
+else
+  SWIFT_PACKAGE_SUFFIX=
+  LINKER=ld-linux-x86-64.so.2
+fi
+
+SWIFT_PACKAGE_URL=https://download.swift.org/swift-5.10-release/ubuntu2204$SWIFT_PACKAGE_SUFFIX/swift-5.10-RELEASE/swift-5.10-RELEASE-ubuntu22.04$SWIFT_PACKAGE_SUFFIX.tar.gz
+SWIFT_DIR=/usr/local/swift-5.10-RELEASE-ubuntu22.04$SWIFT_PACKAGE_SUFFIX
+SWIFT=$SWIFT_DIR/usr/bin/swift
 
 ################################################################################
 # Create Debian chroot
@@ -35,7 +50,7 @@ fi
 ################################################################################
 
 if [ ! -d "$CHROOT_DIR$SWIFT_DIR" ]; then
-  curl -L -s $SWIFT_PACKAGE_URL | tar -C $CHROOT_DIR/usr/local -x -v -z 
+  wget -q -O - $SWIFT_PACKAGE_URL | tar -C $CHROOT_DIR/usr/local -x -v -z 
 fi
 echo -e "$SWIFT_DIR/usr/lib/swift/linux\\n$SWIFT_DIR/usr/lib/swift/host" > $CHROOT_DIR/etc/ld.so.conf.d/swift.conf
 /usr/sbin/chroot $CHROOT_DIR sh -c 'apt-get update && apt-get -y install sqlite3 libncurses6 libcurl4 libxml2 binutils libc6-dev libgcc-13-dev git libstdc++-13-dev'
@@ -68,4 +83,4 @@ set -e
 
 mkdir -p $DESTDIR$PREFIX/bin $DESTDIR$PREFIX/lib/$NAME
 cp $CHROOT_DIR$CHROOT_BUILD_DIR/$BIN_NAME $DESTDIR$PREFIX/bin
-cp $CHROOT_DIR/lib/$LINKER $DESTDIR$PREFIX/lib/$NAME
+cp $CHROOT_DIR/lib/$ARCH-linux-gnu/$LINKER $DESTDIR$PREFIX/lib/$NAME
