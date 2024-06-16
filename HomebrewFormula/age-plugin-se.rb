78b4445b82dc5c2d278cb9b19927fd08462ae4e5
Remko Tronçon <remko@el-tramo.be>
2024-06-16 18:10:40 +0200

    homebrew: Bump upstream version

--- HomebrewFormula/age-plugin-se.rb
+++ HomebrewFormula/age-plugin-se.rb
@@ -1,16 +1,15 @@
-# frozen_string_literal: true
-
 class AgePluginSe < Formula
   desc "Age plugin for Apple Secure Enclave"
   homepage "https://github.com/remko/age-plugin-se"
-  url "https://github.com/remko/age-plugin-se/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "2a1eda0866ac75d88cde04f7d1272c1d23a996d708655e9a9372beed5bff24ec"
+  url "https://github.com/remko/age-plugin-se/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "52d9b9583783988fbe5e94bbe72089a870d128a2eba197fc09a95c13926fb27a"
   license "MIT"
   head "https://github.com/remko/age-plugin-se.git", branch: "main"
 
-  depends_on xcode: ["14.0", :build]
   depends_on "scdoc" => :build
+  depends_on xcode: ["14.0", :build]
   depends_on "age" => :test
+  depends_on :macos
   depends_on macos: :ventura
 
   def install
